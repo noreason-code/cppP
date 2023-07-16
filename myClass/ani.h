09cf54224dcb730d94e83866e26947c217ed1154
‘huangshuo’ <1625744287@qq.com>
2023-07-16 17:56:36 +0800

    析构

--- myClass/ani.h
+++ myClass/ani.h
@@ -12,10 +12,17 @@ class Ani
     public:
         void showName();
         void showAge();
-        Ani(string name,int age){
-            m_name = name;
-            m_age = age;
-        }       
+        // Ani(string name,int age){
+        //     m_name = name;
+        //     m_age = age;
+        // }    
+        Ani(){
+            m_name = "hehe";
+            m_age = 4;
+        }   //构造函数名字与类的名字一样。
+        ~Ani(){
+            cout<<"析构函数，销毁对象。"<<endl;
+        }//析构函数，销毁对象，不是new出来的话没啥用。命名规则：类名前面加个~
 };
 
 inline void Ani::showName(){
--- myClass/useani.cpp
+++ myClass/useani.cpp
@@ -5,9 +5,12 @@
 using namespace std;
 
 int main(){
-    Ani mao("kite",3);
-    mao.showName();
-    mao.showAge();
+    // Ani mao("kite",3);
+    Ani gou;//这么写不报错的情况：（1）没有构造函数，用默认的；（2）有构造函数但是没有形参的情况。
+    // mao.showName();
+    // mao.showAge();
+    gou.showName();
+    gou.showAge();
     system("pause");
     return 0;
 }
