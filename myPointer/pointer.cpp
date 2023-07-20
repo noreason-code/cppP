7248946a587c93da6e6f0c62db9db42ed45b414a
‘huangshuo’ <1625744287@qq.com>
2023-07-20 17:43:37 +0800

    终端运行

--- myPointer/pointer.cpp
+++ myPointer/pointer.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main(){
     int num[4] = {1,2,3,4};
-    int* addr = &num[1];    //取数组头位置法1
-    // int* addr = num;    //取数组头位置法2
+    // int* addr = &num[1];    //取数组头位置法1
+    int* addr = num;    //取数组头位置法2
     cout<<"该数组的地址是："<<addr<<endl;
     cout<<"该数组的第一个元素是："<<*addr<<endl;
+    cout<<*(addr+1)<<endl;
 
-    system("pause");
+    // system("pause");
     return 0;
 }
